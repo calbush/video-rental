4392bd7ee901524419150a6736c50bc9d672ce28
Caleb Bush <calebmatthewbush@gmail.com>
2023-03-02 22:45:08 -0600

    Connect app to MongoDB database

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+const mongoDB = "mongodb+srv://calebmatthewbush:mangothesmasher@cluster0.hutomf5.mongodb.net/video_rental?retryWrites=true&w=majority"
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
