ced04c48a17bb13d6159a389062c1e898c99600f
Caleb Bush <calebmatthewbush@gmail.com>
2023-03-02 22:46:09 -0600

    Create schema for director model

---
+++ models/director.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const DirectorSchema = new Schema({
+    first_name: { type: String, required: true, maxLength: 100 },
+    last_name: { type: String, required: true, maxLength: 100 },
+})
+
+DirectorSchema.virtual('name').get(function (){
+    let fullname = ''
+    if (this.first_name && this.last_name){
+        fullname = `${this.first_name} , ${this.last_name}`
+    }
+    return fullname
+})
+
+DirectorSchema.virtual('url').get(function (){
+    return `/catalog/director/${this._id}`
+})
+
+module.exports = mongoose.model('Director', DirectorSchema)
